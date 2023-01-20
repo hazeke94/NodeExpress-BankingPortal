6c351d901bd184186295ff7c7f82ead9a34a87ba
abdul_haziq <bin.mohd.abdul.haziq@accenture.com>
2023-01-21 01:55:16 +0800

    Block 3 File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -8,9 +8,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Read Account
+const accountData = fs.readFileSync(`${__dirname}/json/accounts.json`, "utf8");
+const accounts = JSON.parse(accountData);
+
+// Read User
+const userData = fs.readFileSync(`${__dirname}/json/users.json`, "utf8");
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Index' })
+    res.render('index', { title: 'Account Summary', accounts: accounts })
 });
+
+app.get('/savings', (req, res) =>{
+    res.render('account', { account: accounts.savings })
+})
+
+app.get('/checking', (req, res) =>{
+    res.render('account', { account: accounts.checkings })
+})
+
+app.get('/credit', (req, res) =>{
+    res.render('account', { account: accounts.credit })
+})
+
+app.get('/profile', (req, res)=> {
+    res.render('profile', { user: users[0] })
+})
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!')
